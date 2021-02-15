e87e5a1885bb68ec22eb0b158e35031a0e8449ee
Leonardo Coelho <sil18001@byui.edu>
2021-02-15 18:40:24 -0300

    lets-chat

---
+++ README.md
@@ -0,0 +1,2 @@
+# chat
+This is an application that will put friends together!!!  Let's chat :)
---
+++ public/main/main.Ctrl.js
@@ -0,0 +1,51 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainCtrl', MainCtrl);
+
+  MainCtrl.$inject = ['$scope', '$localStorage', 'socket', 'lodash'];
+
+  function MainCtrl($scope, $localStorage, socket, lodash) {
+    $scope.message ='';
+    $scope.messages = [];
+    $scope.users = [];
+
+    $scope.mynickname = $localStorage.nickname;
+    var nickname = $scope.mynickname;
+
+
+  socket.on('show-message', function(data){
+    console.log(data);
+  });
+
+  socket.emit('get-users');
+
+    socket.emit('get-users');
+
+     socket.on('all-users', function (data) {
+      console.log(data);
+      $scope.users = data.filter(function (item) {
+        return item.nickname !== nickname;
+      });
+    });
+
+    socket.on('message-recieved', function(data){
+      $scope.messages.push(data);
+    })
+
+
+    $scope.sendMessage = function(date){
+      var newMessage = {
+        message: $scope.message,
+        from: $scope.mynickname
+      }
+      socket.emit('send-message', newMessage);
+      $scope.message = '';
+      $scope.messages.push(newMessage);
+    }
+
+
+  };
+})();
